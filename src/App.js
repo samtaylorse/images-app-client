7b012793e1e5184b48a7caaca9adf359ef236f8a
Sam Taylor <95samt@gmail.com>
2019-10-25 08:26:47 -0700

    Add Routes and home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar  } from 'react-bootstrap';
+import Routes from './Routes';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Navbar.Toggle />
           </Navbar.Header>
         </Navbar>
+        <Routes />
       </div>
     </div>
   );
