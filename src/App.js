61e49ce9626606ff9c68b546f8d6da30c94a06bf
Sam Taylor <95samt@gmail.com>
2019-11-02 11:46:58 -0700

    Add Signup and Login links. Add react-router-bootstrap package so bootstrap links navigate without refreshing the page. Add NotFound component. AWS config.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
-import { Navbar  } from 'react-bootstrap';
+import { Nav, Navbar, NavItem  } from 'react-bootstrap';
 import Routes from './Routes';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App container">
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Resizer</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-        </Navbar>
-        <Routes />
-      </div>
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Resizer</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "st-images-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://im55fvmav8.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_LIODmBne0",
+    APP_CLIENT_ID: "345dvllhc77990ubi83kaffbrp",
+    IDENTITY_POOL_ID: "us-west-2:040fe5c9-3b7a-4cfc-8f13-bcddbed3de48"
+  }
+};
