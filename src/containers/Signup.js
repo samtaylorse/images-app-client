d00eef1f14e21f46de330a3c6c48f676c5c83c5a
Sam Taylor <95samt@gmail.com>
2019-11-03 10:22:06 -0800

    Implement signup.

---
+++ src/containers/Signup.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import {
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { useFormFields } from "../libs/hooksLib";
+import { Auth } from "aws-amplify";
+import "./Signup.css";
+
+export default function Signup(props) {
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    confirmationCode: ""
+  });
+  const [showConfirmationForm, setShowConfirmationForm] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return (
+      fields.email.length > 0 &&
+      fields.password.length > 0 &&
+      fields.password === fields.confirmPassword
+    );
+  }
+
+  function validateConfirmationForm() {
+    return fields.confirmationCode.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      await Auth.signUp({
+        username: fields.email,
+        password: fields.password
+      });
+      setIsLoading(false);
+      setShowConfirmationForm(true);
+    } catch(e) {
+      if (e.code === 'UsernameExistsException') {
+        console.log(e);
+        alert('An account with that email already exists. ' +
+          'If your account is not already confirmed then a new confirmation code will be sent.');
+        await resendConfirmationCode();
+      } else {
+        alert(e.message);
+      }
+      setIsLoading(false);
+    }
+  }
+
+  async function resendConfirmationCode() {
+    try {
+      await Auth.resendSignUp(fields.email);
+      setShowConfirmationForm(true);
+    } catch(e) {
+      alert(e.message);
+    }
+  }
+
+  async function handleConfirmationSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+      props.userHasAuthenticated(true);
+      props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
+  }
+
+  function renderConfirmationForm() {
+    return (
+      <form onSubmit={handleConfirmationSubmit}>
+        <FormGroup controlId="confirmationCode" bsSize="large">
+          <ControlLabel>Confirmation Code</ControlLabel>
+          <FormControl
+            autoFocus
+            type="tel"
+            onChange={handleFieldChange}
+            value={fields.confirmationCode}
+          />
+          <HelpBlock>Please check your email for the code.</HelpBlock>
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateConfirmationForm()}
+        >
+          Verify
+        </LoaderButton>
+      </form>
+    );
+  }
+
+  function renderForm() {
+    return (
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="confirmPassword" bsSize="large">
+          <ControlLabel>Confirm Password</ControlLabel>
+          <FormControl
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.confirmPassword}
+          />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Signup
+        </LoaderButton>
+      </form>
+    );
+  }
+
+  return (
+    <div className="Signup">
+      {showConfirmationForm ? renderConfirmationForm() : renderForm()}
+    </div>
+  );
+}
